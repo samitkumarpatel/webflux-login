9bf065da76851b7b4120abb33932b6e54bfe4b3e
samit-maersk <SamitKumar.Patel@maersk.com>
2023-06-21 00:34:31 +0200

    security validation for user

--- src/main/java/com/example/webfluxlogin/WebfluxLoginApplication.java
+++ src/main/java/com/example/webfluxlogin/WebfluxLoginApplication.java
@@ -1,25 +1,41 @@
 package com.example.webfluxlogin;
 
+import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.annotation.Id;
 
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.authorization.AuthenticatedAuthorizationManager;
+import org.springframework.security.authorization.AuthenticatedReactiveAuthorizationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
+
+import java.util.Collection;
+import java.util.Optional;
 
 @SpringBootApplication
 public class WebfluxLoginApplication {
@@ -61,5 +77,67 @@ class CustomWebSecurity {
 				.csrf(csrf -> csrf.disable())
 				.build();
 	}
+	
 }
-record Users(@Id Long id, String username, String password){}
+record Users(@Id Long id, String username, String password) implements UserDetails {
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		return password;
+	}
+
+	@Override
+	public String getUsername() {
+		return username;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+}
+
+interface UsersRepository extends R2dbcRepository<Users, Long>{
+	public Optional<Users> findByUsername(String username);
+}
+@Service
+@RequiredArgsConstructor
+@Slf4j
+class UsersService implements UserDetailsService {
+	private final UsersRepository usersRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) {
+		log.info("loadUserByUsername: {}", username);
+		return usersRepository
+				.findByUsername(username)
+				.orElseThrow(() -> new UsersNotFoundException("User not found"));
+
+	}
+}
+
+@ResponseStatus(HttpStatus.NOT_EXTENDED)
+class UsersNotFoundException extends RuntimeException {
+	public UsersNotFoundException(String message) {
+		super(message);
+	}
+}
